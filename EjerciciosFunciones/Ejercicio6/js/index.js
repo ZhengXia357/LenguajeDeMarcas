48f78b2c76cebd31c0624b17fd0101340b97c6c4
Zheng Xia <zhengxia357@gmail.com>
2024-01-18 20:45:44 +0100

    EjerciciosFunciones v2 (correción ejercicio 6)

--- EjerciciosFunciones/Ejercicio6/js/index.js
+++ EjerciciosFunciones/Ejercicio6/js/index.js
@@ -5,16 +5,16 @@ function piramide() { //No se introducen parámetros porque si hay que pedirlos 
     var num = parseInt(prompt("Introduce el tamaño de la base de la pirámide"));
     var dir = prompt("Introcuce true o false según la orientación que quieras para la pirámide\ntrue para que esté normal\nfalse para que esté invertido");
 
-    if (dir) {
+    if (dir == "true") {
         for (let i = num; i > 0; i--) {
-            for (let j = 0; j <= (num - i); j++) {
+            for (let j = 1; j < i; j++) {
                 document.write(char);
             }
             document.write("<br>")
         }
     } else {
         for (let i = num; i > 0; i--) {
-            for (let j = 1; j < i; j++) {
+            for (let j = 0; j <= (num - i); j++) {
                 document.write(char);
             }
             document.write("<br>")
